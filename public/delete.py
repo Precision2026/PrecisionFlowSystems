3b483a6cebd83ef43ee25f9eeb0b49a567529693
Precision2026 <sales@precisionflowsystems.co.in>
2026-02-09 13:48:23 +0545

    Remove accidentally pushed python files and update gitignore

--- public/delete.py
+++
@@ -1,9 +0,0 @@
-import os
-folder = "/Users/prabhatthapa/precisionflow/public/pipe-fittings"
-
-
-for file in os.listdir(folder):
-    if file.endswith("jpg"):
-        os.remove(os.path.join(folder, file))
-        print(f"removed {file}")
-        
